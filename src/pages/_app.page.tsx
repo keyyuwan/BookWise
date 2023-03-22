f0f077fd4e29d3e01ce62e68c54771ba90d6731c
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-21 22:28:39 -0300

    style: define global styles

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app'
+import { Nunito } from '@next/font/google'
+
+import { globalStyles } from '@/styles/global'
+
+const nunito = Nunito({
+  subsets: ['latin'],
+})
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${nunito.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from './stitches.config'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+
+  body: {
+    backgroundColor: '$gray800',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    font: '400 1rem Nunito, sans-serif',
+  },
+})
